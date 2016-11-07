a0c85d48df109cb1ac3d6251f1b5c58be3922042
Milan Vojnovic <milanvojnovic@gmail.com>
2016-11-07 17:50:47 +0100

    dummy way how to render a form

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import webapp2
 
+form ="""
+<form action="https://www.google.com/search">
+<input name="q">
+<input type="submit">
+</form>
+"""
+
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Udacity!')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
