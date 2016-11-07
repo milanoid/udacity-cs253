102e014aab503de68912a679203072fea3233c91
Milan Vojnovic <milanvojnovic@gmail.com>
2016-11-07 18:06:12 +0100

    print entire request object

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ class HelloWebapp2(webapp2.RequestHandler):
 class TestHandler(webapp2.RequestHandler):
     def get(self):
         q = self.request.get("q")
-        self.response.write(q)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(self.request)
 
 
 app = webapp2.WSGIApplication([
