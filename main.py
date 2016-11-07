cfd5a3544de2ef4077a0ed9aada4c827f93269cd
Milan Vojnovic <milanvojnovic@gmail.com>
2016-11-07 17:36:34 +0100

    set 'Content-Type' response header

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import webapp2
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, Udacity!')
 
 app = webapp2.WSGIApplication([
